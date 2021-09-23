2bae469bb438c72a2f070d6f9dd76c054453ddb6
Soumeya <soumeyahassan47@gmail.com>
2021-09-23 18:52:07 +0100

    write a test red to green

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,13 @@
-import {projectName} from "./index";
+import { projectName } from "./index";
 
-describe('Starter project should', () => {
-  it('output its name', () => {
-    expect(projectName()).toEqual('Kata starter project');
+describe("Starter project should", () => {
+  it("output its name", () => {
+    expect(projectName()).toEqual("Kata starter project");
+  });
+
+  it("has the correct string length ", () => {
+    const result = projectName();
+
+    expect(result.length).toEqual(20);
   });
 });
